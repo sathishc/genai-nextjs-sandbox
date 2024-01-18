df20fa85ffc0fbd3b1d36e6627b674f59271c31f
sathishc <sat.hariharan@gmail.com>
2024-01-18 11:19:23 +0530

    fix syntax

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,8 @@ import amplifyconfig from '@/amplifyconfiguration.json';
 import type { AppProps } from 'next/app';
 import { Authenticator } from '@aws-amplify/ui-react';
 
-Amplify.configure(amplifyconfig,{
+Amplify.configure({
+  ...amplifyconfig,
   ssr: true
 });
 
