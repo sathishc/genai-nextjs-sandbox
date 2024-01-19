d7576daa49a38f00e4bc3249b0262790b7a36666
sathishc <sat.hariharan@gmail.com>
2024-01-19 14:39:53 +0530

    update UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,25 @@ import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import amplifyconfig from '@/amplifyconfiguration.json';
 import type { AppProps } from 'next/app';
-import { Authenticator } from '@aws-amplify/ui-react';
+import {
+	Button,
+	Flex,
+	Text,
+} from '@aws-amplify/ui-react'
+import { Authenticator, AmplifyProvider, WithAuthenticatorProps } from '@aws-amplify/ui-react';
 
 Amplify.configure({
   ...amplifyconfig,
   ssr: true
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps,  }: AppProps, {signOut, user }:WithAuthenticatorProps ) {
   return (
-    <Authenticator>
-      <Component {...pageProps} />
-    </Authenticator>
+      <Authenticator>
+          <Flex justifyContent={'end'}>
+            <Button onClick={signOut}>Sign Out</Button>
+          </Flex>
+          <Component {...pageProps} />
+      </Authenticator>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,50 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {
+	Button,
+	Flex,
+	Link,
+	Text,
+	TextField,
+	View,
+  ScrollView,
+	withAuthenticator,
+} from '@aws-amplify/ui-react'
+import { useEffect, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
-const callAPI = () => {
-  fetch('/api/chat', {
-    method: 'POST',
-    body: JSON.stringify({ input: 'Who are you?' })
-  });
-};
 
 export default function Home() {
+  const [message, setMessage] = useState('')
+  const [response, setResponse] = useState('')
+
+  const callAPI = async () => {
+    const apiResult = await fetch('/api/chat', {
+      method: 'POST',
+      body: JSON.stringify({ input: message })
+    });
+    const apiResponse = await apiResult.json()
+    setResponse(apiResponse.completion)
+  };
+
+  const clearRespose = () => {
+    setResponse("")
+  };
+  
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-     
-     <button onClick={callAPI}>Click me</button>
-    </main>
+    <View>
+			<ScrollView width="80%" height="500px" backgroundColor="white" border="1px" borderColor="gray">
+        {response}
+      </ScrollView>
+			<TextField
+					width="80%"
+					label="Ask a question and submit" labelHidden={true} 
+					onChange={(e) => setMessage(e.target.value)}
+				/>
+			
+      <Button onClick={callAPI}>Submit Query</Button>
+			<Button onClick={clearRespose}>Clear Response</Button>
+      
+		</View>
+    
+    
   )
 }
